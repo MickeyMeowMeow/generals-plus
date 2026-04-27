c41dd9cb8935bb57a58fe0da3b0157a984d82f0d
zesty_fox <zhs24@mails.tsinghua.edu.cn>
2026-04-27 14:28:07 +0800

    build(client,deps): setup tailwind v4 and shadcn/ui

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,9 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 import { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactRouter(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [
+    reactRouter(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
 });
